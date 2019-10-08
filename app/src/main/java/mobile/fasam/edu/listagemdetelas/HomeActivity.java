bca48dcba411ca8616947982bae9368ce4584bf7
renatorsdeolivera <renatorsdeoliveira@gmail.com>
2019-10-08 14:06:03 -0300

    definido homeAvtivity como tela pricipal e inserido botão para levar para a tela pessoa

---
+++ app/src/main/java/mobile/fasam/edu/listagemdetelas/HomeActivity.java
@@ -0,0 +1,29 @@
+package mobile.fasam.edu.listagemdetelas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void mudarTela(View view) {
+        int opcao = view.getId();
+        Intent intent;
+
+        switch (opcao){
+            case R.id.btnMudar:
+                intent = new Intent(this,PessoaActivity.class);
+                startActivity(intent);
+
+            break;
+        }
+    }
+}
